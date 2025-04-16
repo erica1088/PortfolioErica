20af8142e32c9e5b39fcd0e78f0e5c18345e6be7
erica <acosta.s.erica@gmail.com>
2025-04-16 19:19:06 -0300

    changes

--- src/.Service/ContactFormService.js
+++ src/.Service/ContactFormService.js
@@ -1,17 +1,7 @@
-
-import { collection, addDoc, serverTimestamp } from "firebase/firestore";
-import { db } from "../firebase/Config"; 
+import { db } from './firebase';
+import { collection, addDoc } from 'firebase/firestore';
 
 export const sendMessage = async (formData) => {
-  try {
-    await addDoc(collection(db, "messages"), {
-      ...formData,
-      createdAt: serverTimestamp(),
-    });
-  } catch (error) {
-    console.error("Error al enviar el mensaje: ", error);
-    throw error;
-  }
-
-
-}
+  const messagesRef = collection(db, 'messages');
+  await addDoc(messagesRef, formData);
+};
